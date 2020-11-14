255778bcc2ace3af96dbb0542363c8166772bc4a
Ilan <36084092+ilanolkies@users.noreply.github.com>
2020-11-14 15:42:06 -0300

    Fix LGTM alert
    https://lgtm.com/projects/g/ilanolkies/electron-react-ts/snapshot/6e30ff39109c4c90b49ef00a2fd471ab31cce89c/files/electron/main.ts#xea533bf17bee4d9c:1

--- electron/main.ts
+++ electron/main.ts
@@ -9,8 +9,7 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-      webSecurity: false
+      nodeIntegration: true
     }
   })
 
