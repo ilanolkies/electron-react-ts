12438a820231eb17f603e30c1a3013d899830012
Ilan <36084092+ilanolkies@users.noreply.github.com>
2020-11-15 20:18:32 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ import ReactDom from 'react-dom'
 import { App } from './App'
 import './index.scss'
 
-ReactDom.render(<App />, document.body)
+const root = document.createElement('div')
+ReactDom.render(<App />, root)
